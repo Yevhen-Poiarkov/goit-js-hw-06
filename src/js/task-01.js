aa74cd040205c5daa66af008bdddabe6d142a6f5
Yevhen-Poiarkov <e.poiarkov@gmail.com>
2022-09-08 19:16:20 +0300

    task-2

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,16 +1,10 @@
-const itemRef =
-  document.querySelectorAll(".item");
-console.log(
-  `Number of categories: ${itemRef.length}`
-);
+const itemRef = document.querySelectorAll(".item");
+console.log(`Number of categories: ${itemRef.length}`);
 
-itemRef.forEach((item) => {
-  console.log(
-    `Category: ${item.firstElementChild.textContent}`
-  );
-  const itemList =
-    item.querySelectorAll("li");
-  console.log(
-    `Elements: ${itemList.length}`
-  );
+itemRef.forEach(item => {
+	console.log(
+		`Category: ${item.firstElementChild.textContent}`,
+	);
+	const itemList = item.querySelectorAll("li");
+	console.log(`Elements: ${itemList.length}`);
 });
--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,18 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+	"Potatoes",
+	"Mushrooms",
+	"Garlic",
+	"Tomatos",
+	"Herbs",
+	"Condiments",
 ];
+
+const headingRef = document.querySelector("#ingredients");
+
+const listItem = ingredients.map(ingredient => {
+	const itemEL = document.createElement("li");
+	itemEL.classList.add("item");
+	itemEL.append(ingredient);
+	return itemEL;
+});
+headingRef.append(...listItem);
