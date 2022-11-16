e0b0123beab8186443067ca525bc963cbd560b1e
Yevhen-Poiarkov <e.poiarkov@gmail.com>
2022-11-16 20:57:20 +0200

    task8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,25 @@
+const formRef = document.querySelector(".login-form");
 
+formRef.addEventListener("submit", checkFields);
+
+function checkFields(event) {
+
+	event.preventDefault();
+
+	const formEl = event.currentTarget.elements;
+	const email = formEl.email.value;
+	const password = formEl.password.value;
+
+	if (email.length === 0 || password.length === 0) {
+		alert("Заповніть всі поля");
+		return;
+	}
+
+	const formData = {
+		email,
+		password,
+	};
+	console.log(formData);
+
+	formRef.reset();
+}
